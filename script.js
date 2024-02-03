cd3ec28d82df11cf45bf0030b0c83d51af2365db
virtualka2024 <157579287+virtualka2024@users.noreply.github.com>
2024-02-03 20:45:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// script.js
+
+const ingredientsData = {1: ['Грибы эноки, грибы обжаренные с маринованной говядиной в сладко-остром соусе'],
+  2: ['Кимпаб Ли Минхо', 'Любимый Кимпаб Ли Ноу из Stray Kids, с начинкой из омлета вместо риса, копчёной курицей и майонезом'],
+  3: ['Кимпаб', 'Лист нори, тонкий слой риса, шпинат, морковь, свежий огурец, острая капуста кимчи, омлет томого, охотничьи колбаски, подаётся с бульоном.'],
+  4: ['Корн-дог дошик', 'Сосиска с сыром моцарелла в панировке из дошика'],
+  5: ['Корн-дог фри', 'Сосиска с сыром моцарелла в панировке из картошки фри'],
+  6: ['Корн-дог хлопья', 'Сосиска с сыром моцарелла в панировке из хлопьев'],
+};
+
+function showIngredients(productId) {
+  const ingredientsContainer = document.getElementById('ingredients');
+  const ingredientsList = document.getElementById('ingredients-list');
+
+  ingredientsList.innerHTML = '';
+
+  if (ingredientsData[productId]) {
+    ingredientsData[productId].forEach((ingredient) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = ingredient;
+      ingredientsList.appendChild(listItem);
+    });
+  } else {
+    const listItem = document.createElement('li');
+    listItem.textContent = 'Состав не указан';
+    ingredientsList.appendChild(listItem);
+  }
+
+  ingredientsContainer.style.display = 'block';
+}
+
+function closeIngredients() {
+  const ingredientsContainer = document.getElementById('ingredients');
+  ingredientsContainer.style.display = 'none';
+}
